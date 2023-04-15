8367996e4b14032c8cb237489c51b31cddcbbd00
DylanRhinehart <dylanrhinehart@gmail.com>
2023-04-15 03:43:24 -0700

    landing page styling and animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+import pfp from './images/pfp.JPG';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme}>
+      <div className="background centered">
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className="toggle">
+          Toggle Theme
+        </button>
+        <div className="centered-col full">
+          <div className="centered-row pad-all-5">
+            <img src={pfp} className="pfp rotateGrow"/>
+            <div>
+              <h1 className="text title">Hello</h1>
+              <h3 className="text fadeIn-3 margin-top-btm-0">Nice To Meet You, My Name Is</h3>
+              <h2 className="slideUp font-md anim-text margin-top-btm-0">Dylan Rhinehart</h2>
+              <h4 className="text growUp font-sm margin-top-btm-0">FullStack Software Engineer</h4>
+            </div>
+          </div>
+          <div className="btm-rgt">
+              <a href="https://github.com/Shadowfear36"><BsGithub size={35}/></a>
+              <a href="https://www.linkedin.com/in/dylan-rhinehart/"><BsLinkedin size={35}/></a>
+              <a href="mailto:dylanrhinehart@gmail.com"><MdEmail size={40}/></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
